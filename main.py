4d3233a53ca3c3a0e1be424a8dcface26126bd01
Компьютер <mikegib@mail.ru>
2021-10-25 15:40:33 +0300

    Доработал ТЗ, добавил часть кода
    Доработал ТЗ, добавил часть кода

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print('assd')
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QDialog
+
+
+class Main_Form(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.sign_in = uic.loadUi('Designs(примерно)/sign_in.ui', self)
+        self.btn_registration.clicked.connect(self.open_registration)
+
+    def open_registration(self):
+        self.registration_form = Registration_Form()
+        self.registration_form.show()
+
+
+class Registration_Form(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('Designs(примерно)/registration.ui', self)
+
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = Main_Form()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
